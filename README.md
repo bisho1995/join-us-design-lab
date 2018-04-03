f14f3c0471ddbd50c43874973b6bff78dd5950e9
bisho1995 <bishoatiem@gmail.com>
2018-04-03 17:10:10 +0000

    added register pm route

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 https://flatuicolors.com/palette/cn
+
+#Time Issue
+Suppose the start time is 1 to 12 then no problem
+from 15 to 22 no problem
+but from 23 to 5 what to do ?
+I am suggesting do something like 24 + 5
--- controller/admindashboard.js
+++ controller/admindashboard.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 var managepm = require('./admindashboard/managepm');
 var managecounselor = require('./admindashboard/managecounselor');
+var registerpm = require('./admindashboard/registerpeermotivator');
 
 router.use('/manage-peer-motivator', managepm);
 router.use('/manage-counselor-information', managecounselor);
+router.use('/register-peer-motivator', registerpm);
 
 router.get('/', (req, res, next)=>{
 	res.render('admindashboard/dashboard');
