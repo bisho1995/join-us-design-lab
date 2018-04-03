c0b29b09cfda7ee7b36ac6ba073efa39a0b8d0e7
bisho1995 <bishoatiem@gmail.com>
2018-04-03 15:40:23 +0000

    added registerpm route

--- routes.js
+++ routes.js
@@ -8,12 +8,14 @@ var admindashboard = require('./controller/admindashboard');
 var registerPeerMotivator = require('./controller/admindashboard/registerpeermotivator');
 
 
+
 router.use('/', index);
 router.use('/register', register);
 router.use('/login', login);
 router.use('/client-dashboard/find-counselor', findCounselor);
 router.use('/admin-dashboard', admindashboard);
 router.use('/admin-dashboard/register-peer-motivator', registerPeerMotivator);
+
 
 
 
