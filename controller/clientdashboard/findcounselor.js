a1ee29bb40b049df7c83247d1caee9ec1b6df57a
bisho1995 <pritammukherjee16@gmail.com>
2018-04-08 21:02:27 +0530

    sent logic to another class file

--- controller/clientdashboard/findcounselor.js
+++ controller/clientdashboard/findcounselor.js
@@ -1,49 +1,25 @@
-const router = require('express').Router();
-var request = require('request');
+'use strict'
+const router = require('express').Router()
+var request = require('request')
+const winston = require('../../shared/logger')
 
+const FindCounselor = require('./findCounselorClass')
+const findCounselor = new FindCounselor()
 
-var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?radius=5000&type=physiotherapist&key=AIzaSyDJ4HQrwh2L_J3Zc1W2jQC88K4-TU8TJws&location=';
 
 router.get('/', (req, res, next)=>{
 	res.render('clientdashboard/find_counselor');
 });
 
-router.post('/', (req, res, next)=>{
+router.post('/',async (req, res, next)=>{
+	try{
+		let data =await findCounselor.postReqWork(req.body.location, req.body.pagetoken, req.body.keyword)
+		res.send(data)
+	}catch(err){
+		winston.error(err.stack)
+	}
 
-	let _url = url + getLocation(req.body.location);
-	_url = _url + '&pagetoken=' + getPageToken(req.body.pagetoken);
-	_url = _url + '&keyword=' + getKeyword(req.body.keyword);
-	getPlacesDataFromGoogle(_url)
-		.then(data=>res.json(data))
-		.catch(err=>res.json(err));
-	
 });
 
-
-function getKeyword(keyword){
-	return (keyword === null || keyword === "null" || keyword === undefined || keyword === "undefined" || keyword === "") ? '' : keyword;
-}
-
-function getPageToken(token){
-	return (token === null || token === "null" || token === undefined || token === "undefined" || token === "") ? '' : token;
-}
-
-function getPlacesDataFromGoogle(_url){
-	return new Promise((resolve, reject)=>{
-		request(_url, (err, resp, body)=>{
-			if(err){
-				reject(err);
-			}
-			else{
-				resolve(JSON.parse(body));
-			}
-		});
-	});
-}
-
-
-function getLocation(loc){
-	return (loc === null || loc === "null" || loc === undefined || loc === "undefined" || loc === "") ? '22.530889,88.383418' : loc;
-}
 
 module.exports = router;
