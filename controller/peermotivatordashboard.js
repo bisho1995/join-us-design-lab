fa334c3799a3c83eb6e4a58da5615ad1924d287a
bisho1995 <pritammukherjee16@gmail.com>
2018-04-20 20:28:01 +0530

    add appointments routeforpmdashboard

--- controller/peermotivatordashboard.js
+++ controller/peermotivatordashboard.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
 
+const appointments = require('./peermotivatordashboard/appointments')
+
+router.use('/appointments', appointments)
+
 router.get('/', (req, res, next)=>{
     res.render('pmdashboard/index')
 })
