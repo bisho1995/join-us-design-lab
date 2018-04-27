a7f1ae887c783201b7d498cbe35d032bc7a8a23b
bisho1995 <pritammukherjee16@gmail.com>
2018-04-27 23:00:19 +0530

    added notes route

--- controller/peermotivatordashboard.js
+++ controller/peermotivatordashboard.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 
 const appointments = require('./peermotivatordashboard/appointments')
+const notes = require('./peermotivatordashboard/notes')
 
 router.use('/appointments', appointments)
+router.use('/notes', notes)
 
 router.get('/', (req, res, next)=>{
     res.render('pmdashboard/index')
