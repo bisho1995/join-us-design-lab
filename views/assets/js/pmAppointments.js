e54770789c8705e640f23386dea7735694ed71c9
bisho1995 <pritammukherjee16@gmail.com>
2018-05-01 07:17:30 +0530

    pop open a new window when a link is clicked

--- views/assets/js/pmAppointments.js
+++ views/assets/js/pmAppointments.js
@@ -1,5 +1,14 @@
+
 $(document).ready(()=>{
-    $(".link").each(()=>{
-        console.log($(this).text())
-    })
+    launchHublOnChatButtonClick()
 })
+
+function launchHublOnChatButtonClick(){
+    let links = document.getElementsByClassName("link")
+    for(let i = 0 ; i < links.length ; i++){
+        links[i].addEventListener("click", function(e){
+            let meetingUrl = e.path[0].getAttribute('data-urlMeeting')
+            window.open(meetingUrl, Math.random(), "width=820,height=630,resizeable=yes,location=no")
+        })
+    }
+}
