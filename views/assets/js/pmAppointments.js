6baec003babcbfeb02e9e6860e4b40a9e5d93619
bisho1995 <pritammukherjee16@gmail.com>
2018-05-03 13:53:17 +0530

    working code for notes

--- views/assets/js/pmAppointments.js
+++ views/assets/js/pmAppointments.js
@@ -1,14 +1,28 @@
 
 $(document).ready(()=>{
     launchHublOnChatButtonClick()
+    launchNotesWindowOnClick()
 })
+
+function launchNotesWindowOnClick(){
+    let notesButtons = document.getElementsByClassName("notes")
+    
+    for(let i = 0 ; i < notesButtons.length ; i++){
+        notesButtons[i].addEventListener("click", (e)=>{
+            let client_id = e.path[0].getAttribute('data-client_id')
+            let notesUrl = 'http://localhost:3000/peermotivator-dashboard/notes' + '?client=' + client_id
+            
+            window.open(notesUrl, Math.random(), "width=420,height=315,resizeable=no,location=no")
+        })
+    }
+}
 
 function launchHublOnChatButtonClick(){
     let links = document.getElementsByClassName("link")
     for(let i = 0 ; i < links.length ; i++){
         links[i].addEventListener("click", function(e){
             let meetingUrl = e.path[0].getAttribute('data-urlMeeting')
-            window.open(meetingUrl, Math.random(), "width=820,height=630,resizeable=yes,location=no")
+            window.open(meetingUrl, Math.random(), "left=600,width=820,height=630,resizeable=yes,location=no")
         })
     }
 }
